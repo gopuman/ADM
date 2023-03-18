21e700ba40072204eba143ec23621d7286636f9d
Mahek Jain <53832687+Mahek-Jain@users.noreply.github.com>
2023-03-18 16:30:28 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,18 +1,47 @@
 import json
+import os
 from base64 import b64decode
 from pathlib import Path
 
-DATA_DIR = Path.cwd() / "responses"
-JSON_FILE = DATA_DIR / "An ec-1667994848.json"
-IMAGE_DIR = Path.cwd() / "images" / JSON_FILE.stem
+DATA_DIR = os.getcwd() + "/responses/"
+folder_path = DATA_DIR
+print(folder_path)
+# exit()
 
-IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+# Get a list of all the files in the main folder
+files = os.listdir(folder_path)
+# Filter the list to only include JSON files
+json_files = [folder_path + f for f in files if f.endswith(".json")]
+print(json_files)
+# Process each JSON file as it is created
+for JSON_FILE in json_files:
+    # Open the JSON file and load the data
+    with open(JSON_FILE, mode="r", encoding="utf-8") as file:
+        response = json.load(file)
+        print(response)
+# JSON_FILE = DATA_DIR / "the h-1679043229.json"
+IMAGE_DIR = os.getcwd() +"/images/" #+ JSON_FILE.stem
+CHECK_FOLDER = os.path.isdir(IMAGE_DIR)
 
-with open(JSON_FILE, mode="r", encoding="utf-8") as file:
-    response = json.load(file)
+# If folder doesn't exist, then create it.
+if not CHECK_FOLDER:
+    os.mkdir(IMAGE_DIR)
+    print("created folder : ", IMAGE_DIR)
 
-for index, image_dict in enumerate(response["data"]):
-    image_data = b64decode(image_dict["b64_json"])
-    image_file = IMAGE_DIR / f"{JSON_FILE.stem}-{index}.png"
-    with open(image_file, mode="wb") as png:
-        png.write(image_data)
+else:
+    print(IMAGE_DIR, "folder already exists.")
+
+# IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+print(IMAGE_DIR)
+# exit()'
+for IDX,JSON_FILE in enumerate(json_files):
+
+    with open(JSON_FILE, mode="r", encoding="utf-8") as file:
+        response = json.load(file)
+        # print(file.name)
+
+        for index, image_dict in enumerate(response["data"]):
+            image_data = b64decode(image_dict["b64_json"])
+            image_file = IMAGE_DIR +"/"+ "{}.png".format(IDX)
+            with open(image_file, mode="wb") as png:
+                png.write(image_data)
