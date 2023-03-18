e8a7c316631a5b4dd54828320602e2c3a07f116d
Mahek Jain <53832687+Mahek-Jain@users.noreply.github.com>
2023-03-18 16:22:44 -0700

    Update create.py

--- create.py
+++ create.py
@@ -1,24 +1,47 @@
 import json
 import os
+from base64 import b64decode
 from pathlib import Path
 
-import openai
+DATA_DIR = os.getcwd() + "/responses/"
+folder_path = DATA_DIR
+print(folder_path)
+# exit()
 
-PROMPT = "An eco-friendly computer from the 90s in the style of vaporwave"
-DATA_DIR = Path.cwd() / "responses"
+# Get a list of all the files in the main folder
+files = os.listdir(folder_path)
+# Filter the list to only include JSON files
+json_files = [folder_path + f for f in files if f.endswith(".json")]
+print(json_files)
+# Process each JSON file as it is created
+for JSON_FILE in json_files:
+    # Open the JSON file and load the data
+    with open(JSON_FILE, mode="r", encoding="utf-8") as file:
+        response = json.load(file)
+        print(response)
+# JSON_FILE = DATA_DIR / "the h-1679043229.json"
+IMAGE_DIR = os.getcwd() +"/images/" #+ JSON_FILE.stem
+CHECK_FOLDER = os.path.isdir(IMAGE_DIR)
 
-DATA_DIR.mkdir(exist_ok=True)
+# If folder doesn't exist, then create it.
+if not CHECK_FOLDER:
+    os.mkdir(IMAGE_DIR)
+    print("created folder : ", IMAGE_DIR)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+else:
+    print(IMAGE_DIR, "folder already exists.")
 
-response = openai.Image.create(
-    prompt=PROMPT,
-    n=1,
-    size="256x256",
-    response_format="b64_json",
-)
+# IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+print(IMAGE_DIR)
+# exit()'
+for IDX,JSON_FILE in enumerate(json_files):
 
-file_name = DATA_DIR / f"{PROMPT[:5]}-{response['created']}.json"
+    with open(JSON_FILE, mode="r", encoding="utf-8") as file:
+        response = json.load(file)
+        # print(file.name)
 
-with open(file_name, mode="w", encoding="utf-8") as file:
-    json.dump(response, file)
+        for index, image_dict in enumerate(response["data"]):
+            image_data = b64decode(image_dict["b64_json"])
+            image_file = IMAGE_DIR +"/"+ "{}.png".format(IDX)
+            with open(image_file, mode="wb") as png:
+                png.write(image_data)
